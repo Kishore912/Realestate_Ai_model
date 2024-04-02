d7a3742666062bb6eb588493e9bfbaeb83ea3652
tmachine <tmachine@SYS22.PEC.IN>
2024-04-02 13:52:11 +0530

    allowed host set

--- house_prices/house_prices/settings.py
+++ house_prices/house_prices/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8tzzj8gvc*l3sh7c-lvhgve4y#xw4w-zfbo)#uuft2v)za!^kf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','house-prediction-system.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1','realestate-ai-model.onrender.com']
 
 
 # Application definition
